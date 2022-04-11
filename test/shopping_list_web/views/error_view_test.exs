a3cee9b3da31e22855d04c1793f5e3c082205870
Cyytrus <antoniopaulo122@gmail.com>
2022-04-11 17:31:24 -0300

    Initial commit

---
+++ test/shopping_list_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule ShoppingListWeb.ErrorViewTest do
+  use ShoppingListWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(ShoppingListWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(ShoppingListWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
